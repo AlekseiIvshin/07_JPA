8a89cf4367d39384e36aeccfe1260b11222e17ef
Ivshin Aleksei <ivshinalekseim@mail.ru>
2014-09-10 16:51:37 +0400

    Add store dao

---
+++ src/main/java/store/repository/StoreRepository.java
@@ -0,0 +1,15 @@
+package store.repository;
+
+import java.util.List;
+
+import marks.dao.Mark;
+
+/**
+ * Store repository.
+ * @author Aleksei_Ivshin
+ *
+ */
+public interface StoreRepository {
+	
+	
+}
